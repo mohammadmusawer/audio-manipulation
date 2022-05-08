1f91ebb382e7edba6a8dcc8e50aa79424a122419
Mohammad Musawer <mohammad.musawer@yahoo.com>
2022-05-07 21:57:29 -0400

    Added ability to download a YouTube video and convert to mp3

--- audio_manip.py
+++ audio_manip.py
@@ -1,7 +1,26 @@
+import os
+from pytube import YouTube
+from os import path
 
 def main():
-    print('Hello, World!')
+    track_url = input('Input URL of track to download: ')
+    yt = YouTube(str(track_url))
+
+    print(f'Downloading {yt.title}...')
+
+    # Convert video to audio (mp3)
+    video_to_audio = yt.streams.filter(only_audio=True).first()
+
+    download_destination = '.'
+
+    output_file = video_to_audio.download(output_path=download_destination)
+
+    base, ext = path.splitext(output_file)
+    new_file = base + '.mp3'
+    os.rename(output_file, new_file)
+
+    print('Downloaded track successfully.')
+
 
 if __name__ == '__main__':
     main()
-
