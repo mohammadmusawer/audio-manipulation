a9cdfafdf538149f92bed00cb77953258efcced9
Mohammad Musawer <mohammad.musawer@yahoo.com>
2022-05-07 23:39:44 -0400

    Added error handling for file creation

--- audio_manip.py
+++ audio_manip.py
@@ -8,42 +8,54 @@ from pydub import AudioSegment
 def download_mp3(url):
 
     yt = YouTube(str(url))
-    print(f'Downloading {yt.title}...')
+    print(f'\nDownloading {yt.title}...\n')
 
-    video_to_audio = yt.streams.filter(only_audio=True).first()
-    download_destination = ''
+    # Download mp3 file if it doesn't exist already
+    if not path.exists(yt.title + '.mp3'):
+        video_to_audio = yt.streams.filter(only_audio=True).first()     # Convert from mp4 to mp3
+        download_destination = '.'
+        output_file = video_to_audio.download(output_path=download_destination)
 
-    output_file = video_to_audio.download(output_path=download_destination)
+        base, ext = path.splitext(output_file)
+        new_file = base + '.mp3'
 
-    #TODO: Check for mp3 file to make sure it downloaded successfully by checking .mp3 file exists
-    print('Track downloaded successfully.')
+        os.rename(output_file, new_file)
 
-    return output_file
+        print('Track downloaded successfully.\n')
+        return new_file
+
+    else:
+        print('MP3 file exists already.\n')
+
 
 # Function to convert mp3 to wav format #
 def mp3_to_wav(input_file):
-    segment_audio = AudioSegment.from_file(input_file)
-    segment_audio.export('temp.wav', format='wav')
 
-    #TODO: check if conversion was done successfully by checking if .wav file exists
-    print('Converted from mp3 to wav successfully\n')
+    wav_file_name = 'temp.wav'
+
+    # Convert from mp3 to wav if it doesn't exist already
+    if not path.exists(wav_file_name):
+        segment_audio = AudioSegment.from_file(input_file)
+        segment_audio.export(wav_file_name, format='wav')
+        print('\nConverted from mp3 to wav successfully\n')
+    else:
+        print('\nWAV file exists already.\n')
 
 
 def main():
-    track_url = input('Input URL of track to download: \n')
-    downloaded_file = download_mp3(track_url)
 
-    base, ext = path.splitext(downloaded_file)
-    new_file = base + '.mp3'
-    os.rename(downloaded_file, new_file)
+    track_url = input('Input URL of track to download: ')
 
-    to_wav = input("Convert to WAV? (Y/N): \n")
+    mp3_filename = download_mp3(track_url)
+    to_wav = input('Convert to WAV? (Y/N): ')
 
+    # Ask if user wants to convert to mp3
     if to_wav == 'Y':
-        mp3_to_wav(new_file)
+        mp3_to_wav(mp3_filename)
     else:
         exit(1)
 
 
 if __name__ == '__main__':
     main()
+
